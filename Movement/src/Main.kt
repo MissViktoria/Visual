806a62b127d261da00df5e9daa51c6cf27cb4985
MissViktoria <victoriasteb55@gmail.com>
2025-10-01 23:17:01 +0700

    Подправила после решения конфликтов

--- Movement/src/Main.kt
+++ Movement/src/Main.kt
@@ -1,15 +1,9 @@
-
-=======
 fun main() {
->>>>>>> Interfaces
     val simulationTime = 10.0
     val timeStep = 0.5
     val movementThreads = mutableListOf<Thread>()
-
-<<<<<<< HEAD
-=======
+    
     val movables: Array<Movable> = arrayOf(
->>>>>>> Interfaces
         Human("Иванов Иван Иванович", 25, 1.8),
         Human("Петров Петр Сергеевич", 30, 2.2),
         Driver("Сидоров Сергей Викторович", 35, 2.0)
@@ -19,10 +13,7 @@ fun main() {
 
     println("СИМУЛЯЦИЯ ДВИЖЕНИЯ")
     println("Время симуляции: $simulationTime секунд")
-<<<<<<< HEAD
-=======
     println("Количество участников: ${movables.size}")
->>>>>>> Interfaces
 
     while (currentTime <= simulationTime) {
         movementThreads.clear()
@@ -30,21 +21,16 @@ fun main() {
         println("\nВремя: %.1f сек".format(currentTime))
         println("-".repeat(70))
 
-<<<<<<< HEAD
-=======
         for (movable in movables) {
             val thread = Thread {
                 movable.move(timeStep)
                 println(movable.getInfo())
->>>>>>> Interfaces
             }
             thread.start()
             movementThreads.add(thread)
         }
 
         movementThreads.forEach { it.join() }
-=======
->>>>>>> Interfaces
         currentTime += timeStep
         Thread.sleep(500)
     }
@@ -54,4 +40,7 @@ fun main() {
     println("\nФинальные позиции участников:")
     println("-".repeat(60))
 
->>>>>>> Interfaces
+    movables.forEach { movable ->
+        println(movable.getFinalPosition())
+    }
+}
